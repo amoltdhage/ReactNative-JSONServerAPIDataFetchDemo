9f0153d246fa3238b506b348e6c5778635806c25
amoldtopnotch <amold@topnotchitconsultant.com>
2023-12-14 15:24:00 +0530

    Update App.tsx with user data functionality and improve styling

--- App.tsx
+++ App.tsx
@@ -1,12 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * 
+//  * @format
+//  */
 
-import React, { useEffect, useState } from 'react'; 
-import { Text, View, FlatList, StyleSheet } from 'react-native';
+// import React, { useEffect, useState } from 'react';
+// import { Text, View, FlatList, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
+
+// interface UserData {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
+
+// const App = () => {
+//   const [data, setData] = useState<UserData[]>([]);
+//   const [isFetching, setIsFetching] = useState(false);
+//   const [showUnfetchButton, setShowUnfetchButton] = useState(false);
+
+//   const fetchUserData = async () => {
+//     try {
+//       setIsFetching(true);
+//       setShowUnfetchButton(false);
+
+//       const url = "http://127.0.0.1:3000/users";
+//       const response = await fetch(url);
+
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+
+//       const result: UserData[] = await response.json();
+//       setData(result);
+//       setShowUnfetchButton(true); // Show Unfetch button after fetching data
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     } finally {
+//       setIsFetching(false);
+//     }
+//   };
+
+//   const fetchUserDataWithDelay = () => {
+//     setIsFetching(true); // Show activity indicator
+//     setTimeout(() => {
+//       fetchUserData();
+//     }, 2000); // Wait for 2 seconds before fetching data
+//   };
+
+//   const unfetchUserData = () => {
+//     setIsFetching(false);
+//     setData([]);
+//     setShowUnfetchButton(false);
+//   };
+
+//   const renderUserItem = ({ item }: { item: UserData }) => (
+//     <View style={styles.userContainer}>
+//       <Text style={styles.userId}>ID: {item.id}</Text>
+//       <Text style={styles.userName}>
+//         Name: {item.name || 'Name not available'}
+//       </Text>
+//       <Text style={styles.userEmail}>
+//         Email: {item.email || 'Email not available'}
+//       </Text>
+//     </View>
+//   );
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.headerText}>Users</Text>
+//       <TouchableOpacity
+//         style={styles.fetchButton}
+//         onPress={() => fetchUserDataWithDelay()}
+//         disabled={isFetching}
+//       >
+//         <Text style={styles.buttonText}>
+//           {isFetching ? 'Fetching...' : 'Fetch User Data'}
+//         </Text>
+//       </TouchableOpacity>
+//       {showUnfetchButton && (
+//         <TouchableOpacity
+//           style={styles.unfetchButton}
+//           onPress={() => unfetchUserData()}
+//           disabled={!data.length}
+//         >
+//           <Text style={styles.buttonText}>Unfetch User Data</Text>
+//         </TouchableOpacity>
+//       )}
+//       {isFetching ? (
+//         <ActivityIndicator size="large" color="#3498db" />
+//       ) : data.length ? (
+//         <FlatList
+//           data={data}
+//           keyExtractor={(item) => item.id.toString()}
+//           renderItem={renderUserItem}
+//           style={styles.flatList}
+//         />
+//       ) : (
+//         <View style={styles.centeredContainer}>
+//           <Text style={styles.emptyText}>
+//             No users found. Please fetch user data again.
+//           </Text>
+//         </View>
+//       )}
+//     </View>
+//   );
+// };
+
+
+import React, { useEffect, useState } from 'react';
+import { Text, View, FlatList, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
 
 interface UserData {
   id: number;
@@ -16,44 +119,103 @@ interface UserData {
 
 const App = () => {
   const [data, setData] = useState<UserData[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [showUnfetchButton, setShowUnfetchButton] = useState(false);
 
-  const getAPIData = async () => {
+  const fetchUserData = async () => {
     try {
+      setIsFetching(true);
+      setShowUnfetchButton(false);
+
       const url = "http://127.0.0.1:3000/users";
       const response = await fetch(url);
-      
+
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
       const result: UserData[] = await response.json();
       setData(result);
+      setShowUnfetchButton(true); // Show Unfetch button after fetching data
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
+      setIsFetching(false);
     }
-  }
+  };
 
-  useEffect(() => {
-    getAPIData();
-  }, []);
+  const fetchUserDataWithDelay = () => {
+    setIsFetching(true); // Show activity indicator
+    setTimeout(() => {
+      fetchUserData();
+    }, 2000); // Wait for 2 seconds before fetching data
+  };
+
+  const unfetchUserData = () => {
+    setIsFetching(false);
+    setData([]);
+    setShowUnfetchButton(false);
+  };
+
+  const renderUserItem = ({ item }: { item: UserData }) => (
+    <View style={styles.userContainer}>
+      <Text style={styles.userId}>ID: {item.id}</Text>
+      <Text style={styles.userName}>
+        Name: {item.name || 'Name not available'}
+      </Text>
+      <Text style={styles.userEmail}>
+        Email: {item.email || 'Email not available'}
+      </Text>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>Users</Text>
-      {data.length ? (
+      {data.length > 0 && (
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>Users Data List</Text>
+        </View>
+      )}
+
+      <View style={styles.centeredContainer}>
+        {!showUnfetchButton && (
+          <TouchableOpacity
+            style={styles.fetchButton}
+            onPress={() => fetchUserDataWithDelay()}
+            disabled={isFetching}
+          >
+            <Text style={styles.buttonText}>
+              {isFetching ? 'Fetching...' : 'Fetch User Data'}
+            </Text>
+          </TouchableOpacity>
+        )}
+
+        {isFetching && <ActivityIndicator size="large" color="#3498db" />}
+
+        {!isFetching && data.length === 0 && (
+          <Text style={styles.emptyText}>
+            No users found. Please fetch user data again.
+          </Text>
+        )}
+      </View>
+
+      {showUnfetchButton && (
+        <TouchableOpacity
+          style={styles.unfetchButton}
+          onPress={() => unfetchUserData()}
+          disabled={!data.length}
+        >
+          <Text style={styles.buttonText}>Unfetch User Data</Text>
+        </TouchableOpacity>
+      )}
+
+      {data.length > 0 && (
         <FlatList
           data={data}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <View style={styles.userContainer}>
-              <Text style={styles.userId}>ID: {item.id}</Text>
-              <Text style={styles.userName}>Name: {item.name}</Text>
-              <Text style={styles.userEmail}>Email: {item.email}</Text>
-            </View>
-          )}
+          renderItem={renderUserItem}
+          style={styles.flatList}
         />
-      ) : (
-        <Text style={styles.emptyText}>No users found.</Text>
       )}
     </View>
   );
@@ -65,13 +227,42 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff',
   },
+  headerContainer: {
+    backgroundColor: '#3498db',
+    paddingVertical: 15,
+    marginBottom: 20,
+    borderRadius: 8,
+  },
   headerText: {
     fontSize: 30,
     fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  fetchButton: {
+    backgroundColor: '#3498db',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
     marginBottom: 20,
   },
-  userContainer: {
+  unfetchButton: {
+    backgroundColor: '#e74c3c',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
     marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  userContainer: {
+    backgroundColor: '#e0e0e0',
+    padding: 15,
+    marginBottom: 15,
+    borderRadius: 8,
   },
   userId: {
     fontSize: 18,
@@ -79,6 +270,7 @@ const styles = StyleSheet.create({
   },
   userName: {
     fontSize: 18,
+    marginBottom: 5,
   },
   userEmail: {
     fontSize: 16,
@@ -87,6 +279,16 @@ const styles = StyleSheet.create({
   emptyText: {
     fontSize: 18,
     color: 'red',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  flatList: {
+    marginTop: 10,
+  },
+  centeredContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
